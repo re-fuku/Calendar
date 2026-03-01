bb23771ed6759825c02247cbd87ef25549a00207
fre0ct20 <freoct20@gmail.com>
2026-03-02 00:47:56 +0900

    キー入力を追加

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ class MyCalendar extends HTMLElement {
         })
     }
 
-    // 年を変更する関数
+    // 年を増減する関数
     changeYear(value) {
         this.year += value;
         this.shadowRoot.querySelector('#current-year').textContent = this.year;
@@ -133,12 +133,20 @@ class MyCalendar extends HTMLElement {
         }
     }
 
+    // イベントを作成する関数
     setUpEventListeners() {
+        // ------------------------------------ //
+        // ➀日付入力画面
+        // ------------------------------------ //
 
         // カレンダーアイコンをクリックした際の処理
         this.shadowRoot.querySelector('#calendarBtn').addEventListener('click', () => {
             this.shadowRoot.querySelector('#calendar').classList.add('show');
         });
+
+        // ------------------------------------ //
+        // ➁カレンダー画面
+        // ------------------------------------ //
 
         // カレンダーの[×]ボタンをクリックした際の処理
         this.shadowRoot.querySelector('#closeModal').addEventListener('click', () => {
@@ -155,10 +163,54 @@ class MyCalendar extends HTMLElement {
             this.changeYear(1);
         });
 
-        // カレンダーの年をクリックした際の処理
+        // カレンダーの年の部分をクリックした際の処理
         this.shadowRoot.querySelector('#current-year').addEventListener('click', (e) => {
             this.shadowRoot.querySelector('#year-modal').classList.add('show');
         });
+
+        // キー入力をした際の処理
+        this.shadowRoot.querySelector('#calendar').addEventListener('keydown', (e) => {
+
+            if (!this.shadowRoot.querySelector('#calendar').classList.contains('show')) return;
+
+            const daysInMonth = new Date(this.year, this.month, 0).getDate(); // その月の日数
+
+            switch (e.key) {
+                case 'ArrowLeft':
+                    e.preventDefault();
+                    this.day = this.day > 1 ? this.day - 1 : this.day;
+                    break;
+                case 'ArrowRight':
+                    e.preventDefault();
+                    this.day = this.day < daysInMonth ? this.day + 1 : this.day;
+                    break;
+                case 'ArrowUp':
+                    e.preventDefault();
+                    this.day = this.day > 7 ? this.day - 7 : 1;
+                    break;
+                case 'ArrowDown':
+                    e.preventDefault();
+                    this.day = this.day <= daysInMonth - 7 ? this.day + 7 : this.day;
+                    break;
+            }
+
+            this.updateCalendar();
+        })
+
+        // ------------------------------------ //
+        // ➂年を選択する画面
+        // ------------------------------------ //
+
+        // 年を選択するラジオボタンを選択した際の処理
+        this.shadowRoot.querySelectorAll('input[name="select_year').forEach(radio => {
+            radio.addEventListener('change', (e) => {
+                this.year = parseInt(e.target.value);
+                this.shadowRoot.querySelector('#current-year').textContent = this.year;
+                this.shadowRoot.querySelector('#year-modal').classList.remove('show');
+                this.updateCalendar();
+            })
+        })
+
     }
 
     // カレンダー全体のHTMLを生成してShadow DOMに描画する
@@ -180,7 +232,7 @@ class MyCalendar extends HTMLElement {
                 </div>
 
                 <!-- ➁カレンダー部分 -->
-                <div id="calendar" class="modal">
+                <div id="calendar" class="modal" tabindex="0">
 
                     <!-- ヘッダー -->
                     <div class="calendar-header">
